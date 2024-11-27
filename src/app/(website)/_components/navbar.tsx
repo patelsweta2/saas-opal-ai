177dd3547d8270bdedcf2685059a73d7c0c2dabd
patelsweta2 <sweta0442@gmail.com>
2024-11-27 16:12:17 +0530

    auth part is complete using clerk

--- src/app/(website)/_components/navbar.tsx
+++ src/app/(website)/_components/navbar.tsx
@@ -1,33 +1,43 @@
-import Link from 'next/link';
-import { Menu,User} from 'lucide-react';  // Menu is from lucide-react
-import Image from 'next/image';
-import React from 'react';
 import { Button } from '@/components/ui/button'
+import { Menu, User } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 
-type Props = {};
+import React from 'react'
+
+type Props = {}
 
 const LandingPageNavBar = (props: Props) => {
   return (
     <div className="flex w-full justify-between items-center">
-        <div className="flex text-3xl font-semibold items-center gap-x-3">
-            <Menu className="w-8 h-8"/>
-            <Image alt="logo" src="/opal-logo.svg" width={40} height={40} />
-        </div>
-        <div className="hidden gap-x-10 items-center lg:flex">
-            <Link href="/" className="bg-[#7320DD] py-2 px-5 font-semibold text-lg rounded-full hover:bg-[#7320DD]/80">
-                Home
-            </Link>
-            <Link href='/pricing'>Pricing</Link>
-            <Link href='/contact'>Contact</Link>
-        </div>
-        <Link href='/auth/sign-in'>
-            <Button className='text-base flex gap-x-2'>
-                <User fill='#000'/>
-                Login</Button>
+      <div className="text-3xl font-semibold flex items-center gap-x-3">
+        <Menu className="w-8 h-8" />
+        <Image
+          alt="logo"
+          src="/opal-logo.svg"
+          width={40}
+          height={40}
+        />
+        Opal
+      </div>
+      <div className="hidden gap-x-10 items-center lg:flex">
+        <Link
+          href="/"
+          className="bg-[#7320DD] py-2 px-5 font-semibold text-lg rounded-full hover:bg-[#7320DD]/80"
+        >
+          Home
         </Link>
-
+        <Link href="/">Pricing</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      <Link href='/auth/sign-in'>
+        <Button className="text-base flex gap-x-2">
+          <User fill="#000" />
+          Login
+        </Button>
+      </Link>
     </div>
-  );
-};
+  )
+}
 
-export default LandingPageNavBar;
+export default LandingPageNavBar
