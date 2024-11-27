f23dfb656ee5b46dcb509ad8c73a84aab17ba409
patelsweta2 <sweta0442@gmail.com>
2024-11-27 16:59:22 +0530

    model creation is complete

---
+++ src/actions/user.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+
+export const onAuthenticateUser = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return { status: 403 };
+    }
+  } catch (error) {}
+};
